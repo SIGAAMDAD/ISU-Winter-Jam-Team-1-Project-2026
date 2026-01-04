e1d409aced130c84a6feb913d617766943db4272
SIGAAMDAD <hankburne@gmail.com>
2026-01-04 01:52:46 -0600

    slowly integrating global state management

---
+++ Source/Game/Player/PlayerAudioPlayer.cs
@@ -0,0 +1,60 @@
+using Godot;
+using Nomad.Core.Events;
+
+namespace Game.Player {
+	/*
+	===================================================================================
+	
+	PlayerAudioPlayer
+	
+	===================================================================================
+	*/
+	/// <summary>
+	/// 
+	/// </summary>
+	
+	public sealed class PlayerAudioPlayer {
+		private readonly Player _owner;
+		private readonly AudioStreamPlayer2D _streamPlayer;
+
+		/*
+		===============
+		PlayerAudioPlayer
+		===============
+		*/
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="owner"></param>
+		/// <param name="animator"></param>
+		public PlayerAudioPlayer( Player owner, PlayerAnimator animator ) {
+			animator.PlayerStartMoving.Subscribe( this, OnStartMoveSound );
+			animator.PlayerStopMoving.Subscribe( this, OnStopMoveSound );
+		}
+
+		/*
+		===============
+		OnStartMoveSound
+		===============
+		*/
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="args"></param>
+		private void OnStartMoveSound( in EmptyEventArgs args ) {
+			
+		}
+
+		/*
+		===============
+		OnStopMoveSound
+		===============
+		*/
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="args"></param>
+		private void OnStopMoveSound( in EmptyEventArgs args ) {
+		}
+	};
+};
